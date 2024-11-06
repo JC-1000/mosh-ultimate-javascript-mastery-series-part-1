90b2419f68fb839c6638f2be13da8ae1914cea71
JC-1000 <juliuschembo@gmail.com>
2024-11-06 05:24:48 -0600

    Update 05- Equality Operators.js

--- 03 Operators/05- Equality Operators.js
+++ 03 Operators/05- Equality Operators.js
@@ -9,7 +9,7 @@ As we seen in the last lectures we have different equality operators
 */
 
 // Strict Equality operator (checks for Type + Value) 
-// In a strict equality operator both values have to be of the same type and mus have the same value
+// In a strict equality operator both values have to be of the same type and must have the same value
 console.log(1 === 1) // This returns true because we are comparing the same type to the same value
 console.log('1' === 1) // This returns false because we are comparing different types, a string to number.
 
